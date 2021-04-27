fb1d994160cf870f74ad71297c4d5fb82e3387dc
Jhan Carlos Carvajal Bastidas <liceo.sur.andino2015@gmail.com>
2021-04-26 22:25:33 -0500

    First of all, I added a new class called Cell, this class represents
    every cell within the matrix with its attributes. On the other hand, I
    added four methods to list every cell as the problem demands, the first
    two method to list the cells when the row number entered by user is even
    and the other two when the row number entered by user is odd

--- src/model/GameGrid.java
+++ src/model/GameGrid.java
@@ -1,22 +1,168 @@
 package model;
 
 public class GameGrid {
-	private int rows;
-	private int columns;
+	private Cell first;
+	private int numRows;
+	private int numColumns;
+	
+	public GameGrid(int numRows, int numColumns) {
+		this.numRows = numRows;
+		this.numColumns = numColumns;
+		createMatrix();
+	}
+	
+	private void createMatrix() {
+		if (numRows % 2 == 0) {
+			
+			first = new Cell(numRows * numColumns);
+			createRow(1, 1, first);
+		} 
+		else {
+			
+			first = new Cell((numRows * numColumns - (numColumns - 1)));
+			createRow2(1, 1, first);
+		}
+	}
+	
+	//methods to create the matrix when the rows number is even---------------------------------
+	private void createRow(int i, int j, Cell currentFirstRow) {
+		createCol(i, j + 1, currentFirstRow, currentFirstRow.getUp());
+		if (i < numRows) {
 
-	public GameGrid(int rows, int columns) {
-		super();
-		this.rows = rows;
-		this.columns = columns;
+			if (i % 2 != 0) {
+					
+				Cell downFirstRow = new Cell(currentFirstRow.getNumber() - (2 * numColumns - 1));
+				downFirstRow.setUp(currentFirstRow);
+				currentFirstRow.setDown(downFirstRow);
+				createRow(i + 1, j, downFirstRow);
+
+			} 
+			else {
+					
+				Cell downFirstRow = new Cell(currentFirstRow.getNumber() - 1);
+				downFirstRow.setUp(currentFirstRow);
+				currentFirstRow.setDown(downFirstRow);
+				createRow(i + 1, j, downFirstRow);
+			}
+		}
 	}
 
-	public int getRows() {
-		return rows;
+	private void createCol(int i, int j, Cell prev, Cell rowPrev) {
+		if (j <= numColumns) {
+				
+			if (i % 2 == 0) {
+					
+				Cell current = new Cell(prev.getNumber() + 1);
+				current.setPrevious(prev);
+				prev.setNext(current);
+
+				if (rowPrev != null) {
+						
+					rowPrev = rowPrev.getNext();
+					current.setUp(rowPrev);
+					rowPrev.setDown(current);
+				}
+				createCol(i, j + 1, current, rowPrev);
+			} 
+			else {
+					
+				Cell current = new Cell(prev.getNumber() - 1);
+				current.setPrevious(prev);
+				prev.setNext(current);
+
+				if (rowPrev != null) {
+						
+					rowPrev = rowPrev.getNext();
+					current.setUp(rowPrev);
+					rowPrev.setDown(current);
+				}
+				createCol(i, j + 1, current, rowPrev);
+			}
+		}
+	}
+	//	-------------------------------------------------------------------------------------------
+	
+	//methods to create the matrix when the rows number is odd---------------------------------
+	private void createRow2(int i, int j, Cell currentFirstRow) {
+		createCol2(i, j + 1, currentFirstRow, currentFirstRow.getUp());
+		if (i < numRows) {
+				
+			if (i % 2 != 0) {
+					
+				Cell downFirstRow = new Cell(currentFirstRow.getNumber() - 1);
+				downFirstRow.setUp(currentFirstRow);
+				currentFirstRow.setDown(downFirstRow);
+				createRow2(i + 1, j, downFirstRow);
+
+			}
+			else {
+					
+				Cell downFirstRow = new Cell(currentFirstRow.getNumber() - (2 * numColumns - 1));
+				downFirstRow.setUp(currentFirstRow);
+				currentFirstRow.setDown(downFirstRow);
+				createRow2(i + 1, j, downFirstRow);
+			}
+		}
+	}
+		
+	
+	private void createCol2(int i, int j, Cell prev, Cell rowPrev) {
+		if (j <= numColumns) {
+				
+			if (i % 2 == 0) {
+					
+				Cell current = new Cell(prev.getNumber() - 1);
+				current.setPrevious(prev);
+				prev.setNext(current);
+
+				if (rowPrev != null) {
+						
+					rowPrev = rowPrev.getNext();
+					current.setUp(rowPrev);
+					rowPrev.setDown(current);
+				}	
+				createCol2(i, j + 1, current, rowPrev);
+			} 
+			else {
+					
+				Cell current = new Cell(prev.getNumber() + 1);
+				current.setPrevious(prev);
+				prev.setNext(current);
+
+				if (rowPrev != null) {
+						
+					rowPrev = rowPrev.getNext();
+					current.setUp(rowPrev);
+					rowPrev.setDown(current);
+				}
+				createCol2(i, j + 1, current, rowPrev);
+			}
+		}
+	}
+    //-----------------------------------------------------------------------------------------
+	
+	public String toString() {
+		String msg;
+		msg = toStringRow(first);
+		return msg;
 	}
 
-	public int getColumns() {
-		return columns;
+	private String toStringRow(Cell firstRow) {
+		String msg = "";
+		if (firstRow != null) {
+			msg = toStringCol(firstRow) + "\n";
+			msg += toStringRow(firstRow.getDown());
+		}
+		return msg;
 	}
 
+	private String toStringCol(Cell current) {
+		String msg = "";
+		if (current != null) {
+			msg = current.toString();
+			msg += toStringCol(current.getNext());
+		}
+		return msg;
+	}
 	
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,10 +1,14 @@
 package ui;
 
+import model.GameGrid;
+
 public class Main {
 
 	public static void main(String[] args) {
-		Menu m = new Menu();
-		m.menuOptions();
+//		Menu m = new Menu();
+//		m.menuOptions();
+		GameGrid gameGrid = new GameGrid(10, 10);
+		System.out.println(gameGrid);
 	}
 
 }
